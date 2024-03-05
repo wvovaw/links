f5aeb32d25cfaf05fd8b77128e0e7f301f9df6ad
wvovaw <vladimir5313375015@gmail.com>
2024-03-05 16:57:10 +0500

    Split docs sections, improve docs nav

--- app/components/DocsAsideNavigation/DocsAsideNavigation.vue
+++ app/components/DocsAsideNavigation/DocsAsideNavigation.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
-const { locale } = useI18n();
-const { data: navigation } = await useAsyncData(`navigation-${locale}`, () => fetchContentNavigation({
-  locale: locale.value,
-}));
+import NavMenuSection from "./NavMenuSection.vue";
+
+const { data: rootNav } = await useAsyncData(`root-nav`, () => queryContent("docs").where({ _dir: { $ne: "components" } }).sort({ title: 1 }).find());
+const { data: componentsNav } = await useAsyncData(`components-nav`, () => queryContent("docs", "components").sort({ title: 1 }).find());
+
 // TODO: Find out how to implement recursive navigation tree
 // https://github.com/nuxt-themes/docus/blob/main/components/docs/DocsAsideTree.vue
-const links = computed(() => navigation.value![0].children);
 </script>
 
 <template>
-  <aside class="sticky top-0 h-100vh p-6 border-(r-1 beerus) bg-gohan transition-transform -translate-x-full sm:translate-x-0">
-    <nav class="overflow-y-auto space-y-8 text-moon-16">
-      <section class="space-y-2">
-        <h2 class="text-moon-18 font-semibold  uppercase">
-          Docs
-        </h2>
-        <ul class="flex flex-col space-y-2">
-          <li v-for="link of links" :key="link._path">
-            <NuxtLink
-              :to="link._path"
-              class="text-moon-14 hover:underline"
-            >
-              {{ link.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </section>
+  <aside class="sticky top-0 py-6 px-4 border-(r-1 beerus) bg-gohan transition-transform -translate-x-full sm:translate-x-0">
+    <nav class="overflow-y-auto">
+      <NavMenuSection :links="rootNav" title="index" />
+      <NavMenuSection :links="componentsNav" title="components" />
     </nav>
   </aside>
 </template>
---
+++ app/components/DocsAsideNavigation/NavMenuSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { UIMenu } from "~ui/Menu";
+
+defineProps<{
+  links: any;
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="ps-2 mt-6 mb-4 text-moon-10-caption font-medium uppercase text-trunks">
+    {{ title }}
+  </div>
+  <ul class="flex flex-col space-y-1">
+    <li v-for="link of links" :key="link._path">
+      <NuxtLink :to="link._path" class="group" active-class="active">
+        <UIMenu.Item class="group-[.active]:bg-heles!">
+          <UIMenu.Title>{{ link.title }}</UIMenu.Title>
+        </UIMenu.Item>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
