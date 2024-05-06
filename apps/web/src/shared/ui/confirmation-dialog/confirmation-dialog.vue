211a83ec9d8589ed8d5a432062243a6ccf572a61
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-05-07 01:30:36 +0500

    feat(web): confirmation dialog is now adaptive

--- apps/web/src/shared/ui/confirmation-dialog/confirmation-dialog.vue
+++ apps/web/src/shared/ui/confirmation-dialog/confirmation-dialog.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { UIButton, UIDialog } from "@links/ui";
+import { UIButton, UIDialog, UISheet } from "@links/ui";
 import { isVNode } from "vue";
 import { useConfirmation } from "./use-confirmation";
 
 const { config, show } = useConfirmation();
+const isDesktop = useMediaQuery("(min-width: 640px)");
+const [UseTemplate, ContentComp] = createReusableTemplate();
+
+function closeIfOpen() {
+  if (show.value)
+    config.value.onCancel();
+}
 </script>
 
 <template>
-  <UIDialog.Root :open="show" :modal="true" @update:open="show && config.onCancel()">
+  <UseTemplate>
+    <template v-if="isVNode(config.content)">
+      <component :is="config.content" />
+    </template>
+    <template v-else>
+      {{ config.content }}
+    </template>
+  </UseTemplate>
+
+  <UIDialog.Root v-if="isDesktop" :open="show" @update:open="closeIfOpen">
     <UIDialog.Content>
       <UIDialog.Header>
         <UIDialog.Title>{{ config.title }}</UIDialog.Title>
@@ -15,13 +31,10 @@ const { config, show } = useConfirmation();
           {{ config.subtitle }}
         </UIDialog.Subtitle>
       </UIDialog.Header>
-      <template v-if="isVNode(config.content)">
-        <component :is="config.content" />
-      </template>
-      <template v-else>
-        {{ config.content }}
-      </template>
-      <UIDialog.Footer>
+      <div class="py-4">
+        <ContentComp />
+      </div>
+      <UIDialog.Footer class="flex gap-2">
         <UIButton variant="ghost" @click="config.onCancel">
           Cancel
         </UIButton>
@@ -31,4 +44,26 @@ const { config, show } = useConfirmation();
       </UIDialog.Footer>
     </UIDialog.Content>
   </UIDialog.Root>
+
+  <UISheet.Root v-else :open="show" @update:open="closeIfOpen">
+    <UISheet.Content side="bottom">
+      <UISheet.Header>
+        <UISheet.Title>{{ config.title }}</UISheet.Title>
+        <UISheet.Subtitle v-if="config.subtitle">
+          {{ config.subtitle }}
+        </UISheet.Subtitle>
+        <div class="py-4">
+          <ContentComp />
+        </div>
+        <UISheet.Footer class="flex gap-2">
+          <UIButton variant="ghost" @click="config.onCancel">
+            Cancel
+          </UIButton>
+          <UIButton @click="config.onConfirm">
+            Confirm
+          </UIButton>
+        </UISheet.Footer>
+      </UISheet.Header>
+    </UISheet.Content>
+  </UISheet.Root>
 </template>
