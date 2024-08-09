b3d581e21bd1c2fd5fd0911df599fa5de2fcc42c
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-08-10 00:43:57 +0500

    feat(web): add page-properties-editor to multilinks-constructor

--- apps/web/src/widgets/multilinks-constructor/ui/block-properties-editor/fields/number-field.vue
+++ apps/web/src/widgets/multilinks-constructor/ui/block-properties-editor/fields/number-field.vue
@@ -1,28 +1,41 @@
 <script setup lang="ts">
-import { UIInput, UIInputHint, UIInputLabel } from "@links/ui";
+import { UIButton, UIInput, UIInputHint, UIInputLabel } from "@links/ui";
 import type { IBlockPropertyNumber } from "~shared/core";
 
 interface ITextFieldProps {
   field: IBlockPropertyNumber;
   id: string;
 }
-defineProps<ITextFieldProps>();
+const props = defineProps<ITextFieldProps>();
 const model = defineModel<IBlockPropertyNumber["value"]>();
+
+/**
+ Increment or decrement field value depending on what 1 or -1 provided
+ */
+function stepValue(modifier: 1 | -1) {
+  const newVal = (model.value ?? 0) + props.field.step * modifier;
+  if (newVal <= props.field.max && newVal >= props.field.min)
+    model.value = newVal;
+}
 </script>
 
 <template>
   <UIInputLabel :for="id">
     {{ field.label }}
   </UIInputLabel>
-  <UIInput
-    :id="id"
-    v-model="model"
-    full-width
-    type="number"
-    :min="field.min"
-    :max="field.max"
-    :step="field.step"
-  />
+  <div class="flex gap-2">
+    <UIButton variant="ghost" icon="i-lucide:minus" icon-pos="right" @click="stepValue(-1)" />
+    <UIInput
+      :id="id"
+      v-model="model"
+      full-width
+      type="number"
+      :min="field.min"
+      :max="field.max"
+      :step="field.step"
+    />
+    <UIButton variant="ghost" icon="i-lucide:plus" icon-pos="right" @click="stepValue(1)" />
+  </div>
   <UIInputHint v-if="field.hint">
     {{ field.hint }}
   </UIInputHint>
---
+++ apps/web/src/widgets/multilinks-constructor/ui/page-properties-editor/index.ts
@@ -0,0 +1,1 @@
+export { default as PagePropertiesEditor } from "./page-properties-editor.vue";
