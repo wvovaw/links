1b3a0904f60ecde7b6b40571241d2cec9d36a4ee
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-08-09 20:40:20 +0500

    feat(web): improve loading state in multilinks list widget

--- apps/web/src/widgets/multilinks-list/ui/multilinks-list.vue
+++ apps/web/src/widgets/multilinks-list/ui/multilinks-list.vue
@@ -4,67 +4,81 @@ import LinkActions from "./link-actions.vue";
 import EmptyState from "./empty-state.vue";
 import { CreateNewLinkDialog, useLinksStore } from "~entities/links";
 import { CreateNewLinkForm } from "~features/links";
+import { LoadingSpinner } from "~shared/ui/loading-spinner";
 
 const linksStore = useLinksStore();
 const { refreshLinksList } = linksStore;
 const { links } = storeToRefs(linksStore);
 
+const [isLoading, setLoading] = useToggle(false);
+
 onMounted(() => {
-  refreshLinksList();
+  setLoading(true);
+  refreshLinksList().finally(() => setLoading(false));
 });
 </script>
 
 <template>
-  <div
-    class="flex flex-1 flex-col gap-4 p-4 sm:gap-8 sm:p-10"
-  >
-    <UITable.Root class="mx-auto max-w-4xl w-full overflow-hidden rounded-moon-s-md bg-gohan shadow-moon-sm">
-      <UITable.Header>
-        <UITable.Head>Id</UITable.Head>
-        <UITable.Head>Title</UITable.Head>
-        <UITable.Head>Status</UITable.Head>
-        <UITable.Head>Actions</UITable.Head>
-      </UITable.Header>
-      <UITable.Body>
-        <UITable.Row v-if="links.length < 1" class="hover:bg-transparent">
-          <UITable.Cell colspan="100%">
-            <EmptyState>
-              <div class="mt-6">
-                <CreateNewLinkDialog>
-                  <!-- TODO: Make the form KeepAlive within dialog between it's open/close states -->
-                  <CreateNewLinkForm />
-                </CreateNewLinkDialog>
+  <ClientOnly>
+    <div
+      class="flex flex-1 flex-col gap-4 p-4 sm:gap-8 sm:p-10"
+    >
+      <UITable.Root class="mx-auto max-w-4xl w-full overflow-hidden rounded-moon-s-md bg-gohan shadow-moon-sm">
+        <UITable.Header>
+          <UITable.Head>Title</UITable.Head>
+          <UITable.Head>Link</UITable.Head>
+          <UITable.Head>Status</UITable.Head>
+          <UITable.Head>Actions</UITable.Head>
+        </UITable.Header>
+        <UITable.Body>
+          <UITable.Row v-if="links.length < 1" class="hover:bg-transparent">
+            <UITable.Cell colspan="100%">
+              <div v-if="isLoading" class="w-full flex flex-col place-items-center gap-3">
+                <LoadingSpinner class="h-16 w-16 text-trunks" />
+                <div class="text-lg font-semibold">
+                  Loading
+                </div>
               </div>
-            </EmptyState>
-          </UITable.Cell>
-        </UITable.Row>
+              <EmptyState v-else>
+                <div class="mt-6">
+                  <CreateNewLinkDialog>
+                    <!-- TODO: Make the form KeepAlive within dialog between it's open/close states -->
+                    <CreateNewLinkForm />
+                  </CreateNewLinkDialog>
+                </div>
+              </EmptyState>
+            </UITable.Cell>
+          </UITable.Row>
 
-        <UITable.Row
-          v-for="link of links" :key="link.id"
-        >
-          <UITable.Cell>{{ link.id }}</UITable.Cell>
-          <UITable.Cell class="w-full font-semibold">
-            {{ link.title }}
-          </UITable.Cell>
-          <UITable.Cell>
-            <UITag
-              class="w-fit"
-              :variant="link.status === 'DRAFT' ? 'outline' : 'fill'"
-            >
-              {{ link.status }}
-            </UITag>
-          </UITable.Cell>
-          <UITable.Cell>
-            <LinkActions :link-id="link.$id" />
-          </UITable.Cell>
-        </UITable.Row>
-      </UITable.Body>
-    </UITable.Root>
-    <div v-if="links.length > 0" class="flex justify-center">
-      <CreateNewLinkDialog>
-        <!-- TODO: Make the form KeepAlive within dialog between it's open/close states -->
-        <CreateNewLinkForm />
-      </CreateNewLinkDialog>
+          <UITable.Row
+            v-for="link of links" :key="link.id"
+          >
+            <UITable.Cell class="w-50% font-semibold">
+              {{ link.title }}
+            </UITable.Cell>
+            <UITable.Cell class="w-50%">
+              {{ link.name.$id }}
+            </UITable.Cell>
+            <UITable.Cell>
+              <UITag
+                class="w-fit"
+                :variant="link.status === 'DRAFT' ? 'outline' : 'fill'"
+              >
+                {{ link.status }}
+              </UITag>
+            </UITable.Cell>
+            <UITable.Cell>
+              <LinkActions :link-id="link.$id" />
+            </UITable.Cell>
+          </UITable.Row>
+        </UITable.Body>
+      </UITable.Root>
+      <div v-if="links.length > 0" class="flex justify-center">
+        <CreateNewLinkDialog>
+          <!-- TODO: Make the form KeepAlive within dialog between it's open/close states -->
+          <CreateNewLinkForm />
+        </CreateNewLinkDialog>
+      </div>
     </div>
-  </div>
+  </ClientOnly>
 </template>
